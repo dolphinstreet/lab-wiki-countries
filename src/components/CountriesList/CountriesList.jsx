f1fe8824eda1463b0d12295cc1e8453281c1490b
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-02 23:51:39 +0100

    styling

--- src/components/CountriesList/CountriesList.jsx
+++ src/components/CountriesList/CountriesList.jsx
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 
 const CountriesList = ({countries})=>{
     return (
-        <ul>
+        <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+        
+        <div className="list-group">
         {countries.map(country =>{
             return(
-                <li key={country.alpha2Code}>
+                <div key={country.alpha2Code} className="list-group-item list-group-item-action">
                     <Link to={country.alpha3Code}>
                         <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}alt="{country.name.common} flag"/>
                         <h3>{country.name.common}</h3>
                     </Link>
-                </li>
+                </div>
             )
         })}
-        </ul>
+        </div>
+        </div>
     )
 
 
--- src/components/CountryDetails/CountryDetails.jsx
+++ src/components/CountryDetails/CountryDetails.jsx
@@ -11,41 +11,41 @@ const CountriesDetails = ({countries})=>{
    
 
     return (
-        <div className="country-details" key={foundCountry.alpha2Code}>
-     
-            <h3>{foundCountry.name.common}</h3>
-            <div className="capital-row"> 
-                <h4>
-                Capital
-                </h4>
-                    <p>{foundCountry.capital[0]}</p>
-            </div>
-            <div className="surface-row"> 
-                <h4>
-                Area
-                </h4>
-                    <p>{foundCountry.area}</p>
-            </div>
-            <div className="borders-row"> 
-            <h4>
-                Borders
-                </h4>
-                <ul >
-                    {foundCountry.borders.map((borderCountryCode) =>{
-                        const foundBorderCountry = countries.find((country) => {   
-                             return country.alpha3Code === borderCountryCode
-                        })
-                        return (
-                            <Link to={"../"+foundBorderCountry.alpha3Code}>
-                                <li key={foundCountry.name.common+foundBorderCountry.name.common}>
-                                    {foundBorderCountry.name.common}
-                                </li>
-                            </Link>
-                        )
-                     })}
-                </ul> 
-            </div>
-
+        <div className="col-7" key={foundCountry.alpha2Code}>
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${foundCountry.alpha2Code.toLowerCase()}.png`}alt="{country.name.common} flag"/>
+            <h1>{foundCountry.name.common}</h1>
+            <table className="table">
+            <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{width: "30%"}}>Capital</td>
+                        <td>{foundCountry.capital[0]}</td>
+                    </tr>
+                    <tr>
+                        <td>Capital</td>
+                        <td>{foundCountry.area}km<sup>2</sup></td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                 {foundCountry.borders.map((borderCountryCode) =>{
+                                    const foundBorderCountry = countries.find((country) => {   
+                                    return country.alpha3Code === borderCountryCode
+                                    })
+                                    return (
+                                        <Link to={"../"+foundBorderCountry.alpha3Code}>
+                                            <li key={foundCountry.name.common+foundBorderCountry.name.common} style={{listStyle:"none"}}>
+                                                {foundBorderCountry.name.common}
+                                            </li>
+                                        </Link>
+                                    )
+                                 })}
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 
@@ -53,3 +53,11 @@ const CountriesDetails = ({countries})=>{
 }
 
 export default CountriesDetails;
+
+
+              
+            
+                
+                
+        
+    
